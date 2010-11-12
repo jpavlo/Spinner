ce83bca05548721aeda485bba34694cea0420233
jpavlo <Administrador@dell>
2010-11-11 19:54:16 -0600

    primero

---
+++ src/org/spinner/test/SpinnerActivity.java
@@ -0,0 +1,24 @@
+package org.spinner.test;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SpinnerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        Spinner s = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.planets, android.R.layout.simple_list_item_1);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+}
